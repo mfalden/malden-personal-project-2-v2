d3c8acda8633b92c9b6a8dffefa1a3f7d02ee559
mfalden <malden2023@chadwickschool.org>
2022-03-07 10:25:26 -0800

    Issue: Space not updating

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class Program
 {
     public static int baseRow = 7;
-    public static int start = 1;
+    public static int start = 0;
     public static int totalLength = 0;
     public static int spaces = 0;
     public static int lastobstaclelength = 0;
@@ -17,11 +17,11 @@ public class Program
         // to put it inside of a while loop to get it to work properly.
         while (true)
         {
-            
+
             //FancyConsole.Write(1, 1, "Hello World!");
 
-                    // obstacles.Add(Obstacle.GetRandomObstacle());
-                    // DrawAllObstacles(obstacles);
+            // obstacles.Add(Obstacle.GetRandomObstacle());
+            // DrawAllObstacles(obstacles);
 
             // TODO(jcollard 2022-03-06): You need to call refresh after you've
             // specified all the things you want to print to the screen. Nothing
@@ -32,28 +32,34 @@ public class Program
 
             bool hasGameStarted = true; // false for testing
             while (hasGameStarted == true)
-                {
-
+            {
+                // FancyConsole.Clear();
                 MoveScreen();
-                        FancyConsole.Refresh();
+                DrawAllObstacles(obstacles);
+                DisplayDebugInfo();
+                FancyConsole.Refresh();
 
-            // TODO(jcollard 2022-03-06): You want to put at least a small delay
-            // at the end of the loop otherwise you will see flickering.
-                        FancyConsole.Sleep(10);
-                 }
+                // TODO(jcollard 2022-03-06): You want to put at least a small delay
+                // at the end of the loop otherwise you will see flickering.
+                FancyConsole.Sleep(10);
+            }
         }
 
     }
 
     public static void InitializeScreen()
     {
-        List<Obstacle> start = new List<Obstacle>();
-        start.Add(Obstacle.GetRandomObstacle());
-        start.Add(Obstacle.GetRandomObstacle());
-        start.Add(Obstacle.GetRandomObstacle());
-        start.Add(Obstacle.GetRandomObstacle());
-        start.Add(Obstacle.GetRandomObstacle());
-        DrawAllObstacles(start);
+        List<Obstacle> obstacles = new List<Obstacle>();
+        int times = 10;
+        while (times > 0)
+        {
+            obstacles.Add(Obstacle.GetRandomObstacle());
+            times = times - 1;
+        }
+        //Obstacle firstElement = obstacles[0];
+
+        DrawAllObstacles(obstacles);
+
     }
 
     public static void MoveScreen()
@@ -66,8 +72,9 @@ public class Program
         char asChar = (char)input;
         if (asChar == ' ')
         {
+
             spaces++;
-            DrawAllObstacles(AddObstacle(obstacles));
+            AddObstacle(obstacles);
         }
     }
 
@@ -76,7 +83,8 @@ public class Program
         if (spaces > totalLength + lastobstaclelength)
         {
             obstacles.Add(Obstacle.GetRandomObstacle());
-            totalLength += lastobstaclelength;
+            // Obstacle lastElement = obstacles[obstacles.Count-1];
+            // totalLength += lastElement.Length;
         }
         if (spaces == 1)
         {
@@ -85,34 +93,44 @@ public class Program
         return obstacles;
     }
 
-// NOTE: DRAWALLOBSTACLES BEING EDITED. REFERENCE "Added Method Exception"
+    // NOTE: DRAWALLOBSTACLES BEING EDITED. REFERENCE "Added Method Exception"
     public static void DrawAllObstacles(List<Obstacle> obstacles)
-    {   
+    {
         foreach (Obstacle o in obstacles)
         {
             int columnNumber = 0;
             int drawnHeight = 0;
             while (columnNumber <= o.Length)
+            {
+                FancyConsole.Write(1, 0, $"Here: {spaces}");
+                while (columnNumber < o.X)
                 {
-                    while (columnNumber < o.X)
-                    {
-                        FancyConsole.Write(baseRow, start - spaces + columnNumber + totalLength, "_");
-                        columnNumber++;
-                    }
-                    if (columnNumber == o.X)
-                        {
-                            while (drawnHeight <= o.Height)
-                                {
-                                    FancyConsole.Write(baseRow - drawnHeight, start - spaces + columnNumber + totalLength, "#");
-                                    drawnHeight++;
-                                }
-                            columnNumber++;
-                        }
+                    FancyConsole.Write(2, 0, $"Here: {spaces}");
+
                     FancyConsole.Write(baseRow, start - spaces + columnNumber + totalLength, "_");
                     columnNumber++;
                 }
-                totalLength += o.Length;
+                if (columnNumber == o.X)
+                {
+                    while (drawnHeight <= o.Height)
+                    {
+                        FancyConsole.Write(3, 0, $"Here: {spaces}");
+                        FancyConsole.Write(baseRow - drawnHeight, start - spaces + columnNumber + totalLength, "#");
+                        drawnHeight++;
+                    }
+                    columnNumber++;
+                }
+                FancyConsole.Write(baseRow, start - spaces + columnNumber + totalLength, "_");
+                columnNumber++;
+            }
+            totalLength += o.Length;
         }
+    }
+
+    public static void DisplayDebugInfo()
+    {
+        FancyConsole.SetColor(FancyColor.WHITE);
+        FancyConsole.Write(0, 0, $"Spaces: {spaces}");
     }
 }
 
